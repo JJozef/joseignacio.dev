e03c1e7b8abc42422dbb392c6bd3a8fca8034a25
JJozef <Josemillaquen12@gmail.com>
2024-03-09 23:21:30 -0300

    remove version 2023

--- src/components/Container.js
+++
@@ -1,7 +0,0 @@
-import clsx from 'clsx'
-
-function Container({ children, className }) {
-  return <article className={clsx('px-4 py-3 max-w-5xl m-auto', className)}>{children}</article>
-}
-
-export default Container
