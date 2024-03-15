7926d90f1d4e6b48f01a0ad6ae5ade3fb2b124a0
JJozef <Josemillaquen12@gmail.com>
2024-03-15 00:08:07 -0300

    Refactor select-theme.js

--- src/components/select-theme.js
+++ src/components/select-theme.js
@@ -3,21 +3,17 @@
 import { MoonIcon, SunIcon } from '@radix-ui/react-icons'
 import { useTheme } from 'next-themes'
 
-import { Button } from './ui/button'
+import { Button } from '@/components/ui/button'
 
 export function SelectTheme() {
   const { theme, setTheme } = useTheme()
 
-  const handleTheme = () => {
-    if (theme === 'dark') {
-      setTheme('light')
-    } else {
-      setTheme('dark')
-    }
-  }
-
   return (
-    <Button variant='outline' className='w-9 px-3' onClick={handleTheme}>
+    <Button
+      variant='outline'
+      className='w-9 px-3'
+      onClick={() => setTheme(theme === 'dark' ? 'light' : 'dark')}
+    >
       {theme === 'dark' ? (
         <SunIcon className='h-4 w-4 min-w-4' />
       ) : (
