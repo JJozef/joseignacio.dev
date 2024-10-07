31b802051196901f1c87b1372ed387ec6d3754c1
Jose Ignacio <josemillaquen12@gmail.com>
2024-10-07 00:23:20 -0300

    fix: images remotePatterns

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    remotePatterns: ['avatars.githubusercontent.com']
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'avatars.githubusercontent.com'
+      }
+    ]
   }
 }
 
